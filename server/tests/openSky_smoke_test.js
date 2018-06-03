ff50db40bdf9bcd11800bcb57eb0712baa96b3cd
Yevhen <ybatsiun@gmail.com>
2018-06-04 01:00:11 +0300

    add icao number unit test

--- server/tests/openSky_smoke_test.js
+++ server/tests/openSky_smoke_test.js
@@ -7,23 +7,42 @@ const request = require('request');
 const auth_helper = require('./helpers/authentication_helper.js');
 
 describe('Open sky network. Smoke', function () {
-    this.timeout(10000);
-    
+    this.timeout(30000);
+    let firstIcaoAvailable, user;
     it('GET /authenticated/icaoList', (done) => {
-        auth_helper.registerUser().then((user) => {
-            request.get('http://localhost:3000/authenticated/icaoList', function (err, response, body) {
-                request({
-                    headers: {
-                      'x-auth': user.token,
-                    },
-                    uri: 'http://localhost:3000/authenticated/icaoList',
-                    method: 'GET'
-                  }, function (err, res, body) {
-                      expect(res.statusCode).to.be(200);
-                      expect(res.body.length).to.be.above(0);
-                    done();
-                  });
+        auth_helper.registerUser().then(registeredUser => {
+            user = registeredUser;
+            request({
+                headers: {
+                    'x-auth': user.token,
+                },
+                uri: 'http://localhost:3000/authenticated/icaoList',
+                method: 'GET'
+            }, function (err, res, body) {
+                expect(res.statusCode).to.be(200);
+                //res.body is a string. transforming it to array
+                const icaoNumbers = res.body.replace(/"|\[|\]/g, "").split(",");
+                expect(icaoNumbers.length).to.be.above(0);
+                firstIcaoAvailable = icaoNumbers[0];
+                done();
             });
         }).catch((e) => done(e));
+    });
+
+    it('Add plane to myIcaoList', (done) => {
+        request({
+            headers: {
+                'x-auth': user.token,
+            },
+            uri: `http://localhost:3000/authenticated/addIcao/${firstIcaoAvailable}`,
+            method: 'POST'
+        }, function (err, res, body) {
+            if (err) {
+                done(e);
+            }
+            debugger
+            expect(res.statusCode).to.be(200);
+            done();
+        });
     });
 });

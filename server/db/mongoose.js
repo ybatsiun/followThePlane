373cd2a4d93f522aef2c6a3076b4560788f1e2a7
Yevhen <ybatsiun@gmail.com>
2018-10-06 00:43:00 +0300

    drop test db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,5 +3,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URI);
+if (mongoose.connection.name.includes('test')) {
+    mongoose.connection.dropDatabase();
+};
 
 module.exports = mongoose;
